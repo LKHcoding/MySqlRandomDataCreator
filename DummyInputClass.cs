6f262d42b0381cdd289dea7787cfeca24cef463c
LKHcoding <tha920@naver.com>
2020-09-18 17:53:27 +0900

    MySql DB 랜덤 데이터 생성기 구현 완료

---
+++ DummyInputClass.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class DummyInputClass
+    {
+        //차례 대로 성,이름,전화번호,메일을 조합하기 위한 문자열        
+        static string[] FirstName = { "박","박", "박","박", "이","이", "김","김","김", "유", "장", "오", "김","김","김", "김",
+                                   "노", "윤","강","최","우","정","하","옥", "권", "권", "권", "권", "임"};
+        static string[] LastName = { "지", "훈", "용", "덕", "종", "은", "남", "준", "석", "영", "현", "창", "민","창", "민","창", "민","창", "민","창", "민",
+            "창", "민","창", "민","창", "민","창", "민","창", "민","창", "민","창", "민","창", "민","창", "민","창", "민","창", "민","창", "민","창", "민",
+            "근", "희","근", "희","근", "희","근", "희","근", "주", "희", "주","주","주","주","주","주","주","희","희","희","희","희","희","희",
+            "지", "용","지", "용","지", "용","지", "용","지", "용","지", "용","지", "용","지", "용","지", "용","지", "용",
+                                   "정", "준", "상", "택", "협", "서", "희","지","훈","진","우","성", "운", "운", "운", "운", "영", "영", "영", "영", "영", "영"};
+        static string[] FirstPhoneNumber = { "010", "010", "010", "011", "019", "017", "016", "018" };
+        static string[] MailSP = { "korea.com", "nate.com", "yahoo.co.kr", "hanmail.net", "daum.net", "naver.com" };
+
+        static string[] FirstAddress = { "서울", "대전", "대구", "부산", "광주", "제주", "포항" };
+        static string[] LastAddress = { "북구", "동구", "서구", "남구", "중구" };
+
+        //count 만큼 이름을 랜덤으로 만들어서 문자열의 배열을 리턴한다.
+        static public string[] GetName(int count)
+        {
+            Random r = new Random();
+            string[] Name = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                Name[i] = string.Format("{0}{1}{2}",
+                    FirstName[r.Next(0, FirstName.Length)],
+                    LastName[r.Next(0, LastName.Length)],
+                    LastName[r.Next(0, LastName.Length)]);
+                if (Name[i][1] == Name[i][2]) i--; // 중간 글자와 세번째 글자가 중복 되면 다시한번 루프를 한번더 돕니다.
+            }
+            return Name;
+        }
+        //count 만큼 전화번호를 만들어서 문자열의 배열을 리턴한다.
+        static public string[] GetPhoneNumber(int count)
+        {
+            Random r = new Random();
+            string[] PhoneNumber = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                PhoneNumber[i] = string.Format("{0}-{1}-{2}",
+                    FirstPhoneNumber[r.Next(0, FirstPhoneNumber.Length)],
+                    r.Next(1111, 10000).ToString(),
+                    r.Next(1111, 10000).ToString());
+            }
+            return PhoneNumber;
+        }
+        //count 만큼 메일 주소를 만들어서 문자열의 배열을 리턴한다.
+        //문자열의 재조합이 빈번?해서 StringBuilder을 사용했습니다.
+        static public string[] GetMailAddress(int count)
+        {
+            Random r = new Random();
+            string[] MailAddress = new string[count];
+            StringBuilder strb = new StringBuilder(12);
+            for (int i = 0; i < count; i++)
+            {
+                int idcount = r.Next(5, 12);
+                for (int j = 0; j < idcount; j++)
+                    strb.Append((char)r.Next(97, 123));
+
+                MailAddress[i] = string.Format("{0}@{1}",
+                    strb, MailSP[r.Next(0, MailSP.Length)]);
+                strb.Remove(0, strb.Length);
+            }
+            return MailAddress;
+        }
+
+        static public string[] GetAddress(int count)
+        {
+            Random r = new Random();
+            string[] Address = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                Address[i] = string.Format("{0} {1} 입니다.",
+                    FirstAddress[r.Next(0, FirstAddress.Length)],
+                    LastAddress[r.Next(0, LastAddress.Length)]);
+                //if (Name[i][1] == Name[i][2]) i--; // 중간 글자와 세번째 글자가 중복 되면 다시한번 루프를 한번더 돕니다.
+            }
+            return Address;
+        }
+        static public int[] GetAge(int count)
+        {
+            Random r = new Random();
+
+            int[] age = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                age[i] = r.Next(20, 70);
+            }
+
+            return age;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,226 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int indexing;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            Init();
+
+            //insert();
+
+            //select();
+        }
+
+        private void select()
+        {
+            using (MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;Database=" + this.DBname.Text + ";Uid=" + this.DBID.Text + ";Pwd=" + this.DBPW.Text + ""))
+            {
+                try//예외 처리
+                {
+                    this.ListBox.Items.Clear();
+                    connection.Open();
+                    string sql = "SELECT * FROM " + this.TableName.Text;
+
+                    //ExecuteReader를 이용하여
+                    //연결 모드로 데이타 가져오기
+                    MySqlCommand cmd = new MySqlCommand(sql, connection);
+
+                    MySqlDataReader table = cmd.ExecuteReader();
+
+                    //List<string> strList = new List<string>();
+
+                    //사용가능한 필드명들 받아오기
+                    var columns = new List<string>();
+
+                    for (int i2 = 0; i2 < table.FieldCount; i2++)
+                    {
+                        columns.Add(table.GetName(i2));
+                    }
+
+                    int aa = 0;
+                    while (table.Read())
+                    {
+                        string Results = "";
+                        for (int ii = 0; ii < columns.Count; ii++)
+                        {
+                            Results += columns[ii] + " : " + table[columns[ii]] + ", ";
+                        }
+
+                        this.ListBox.Items.Add(Results);
+
+                        //this.ListBox.Items.Add("[" + table[this.Column1.Text] + "] 이름 : " + table[this.Column2.Text] + ", 나이 : " + table[this.Column3.Text] + ", 주소 : " + table[this.Column4.Text] + ", 번호 : " + table[this.Column5.Text]/* + ", 메일 : " + table[this.Column6.Text]*/);
+                        if (aa % 10 == 0 && aa != 0)
+                        {
+                            this.ListBox.Items.Add("----------------------------------------------------------------");
+                        }
+                        aa++;
+                        //strList.Add(table["name"]);
+                        //Console.WriteLine("{0} {1}", table["idx"], table["header"], table["body"]);
+                    }
+                    table.Close();
+                    connection.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    this.ListBox.Items.Add(ex);
+                    //Console.WriteLine("실패");
+                    //Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+
+        private void insert()
+        {
+            string[] names = DummyInputClass.GetName(int.Parse(this.howmuch.Text));
+            int[] age = DummyInputClass.GetAge(int.Parse(this.howmuch.Text));
+            string[] address = DummyInputClass.GetAddress(int.Parse(this.howmuch.Text));
+            string[] phoneNo = DummyInputClass.GetPhoneNumber(int.Parse(this.howmuch.Text));
+            string[] email = DummyInputClass.GetMailAddress(int.Parse(this.howmuch.Text));
+
+
+            using (MySqlConnection connection = new MySqlConnection("Server=localhost;Port=3306;Database=" + this.DBname.Text + ";Uid=" + this.DBID.Text + ";Pwd=" + this.DBPW.Text + ""))
+            {
+
+                try//예외 처리
+                {
+                    connection.Open();
+                    for (int i = 0; i < int.Parse(this.howmuch.Text); i++)
+                    {
+
+                        string query = "(";
+
+                        if (this.Chk2.IsChecked.Value == true)
+                        {
+                            query += this.Column2.Text + ",";
+                        }
+                        if (this.Chk3.IsChecked.Value == true)
+                        {
+                            query += this.Column3.Text + ",";
+                        }
+                        if (this.Chk4.IsChecked.Value == true)
+                        {
+                            query += this.Column4.Text + ",";
+                        }
+                        if (this.Chk5.IsChecked.Value == true)
+                        {
+                            query += this.Column5.Text + ",";
+                        }
+                        if (this.Chk6.IsChecked.Value == true)
+                        {
+                            query += this.Column6.Text + ",";
+                        }
+
+                        query += ")";
+                        query = query.Replace(",)", ")");
+
+                        //query = query.Split(",)")[0] + ")";
+
+
+                        string values = "(";
+
+                        if (this.Chk2.IsChecked.Value == true)
+                        {
+                            values += "'" + names[i] + "',";
+                        }
+                        if (this.Chk3.IsChecked.Value == true)
+                        {
+                            values += "'" + age[i] + "',";
+                        }
+                        if (this.Chk4.IsChecked.Value == true)
+                        {
+                            values += "'" + address[i] + "',";
+                        }
+                        if (this.Chk5.IsChecked.Value == true)
+                        {
+                            values += "'" + phoneNo[i] + "',";
+                        }
+                        if (this.Chk6.IsChecked.Value == true)
+                        {
+                            values += "'" + email[i] + "',";
+                        }
+
+                        values += ")";
+                        values = values.Replace(",)", ")");
+                        //values = values.Split(",)")[0] + ")";
+
+
+                        //+" ) values('" + names[i] + "', '" + age[i] + "','" + address[i] + "', '" + phoneNo[i] + "')";
+
+                        //string sql = "SELECT * FROM mem";
+                        string sql = "insert into " + this.TableName.Text + " " + query + " values" + values;
+
+                        //ExecuteReader를 이용하여
+                        //연결 모드로 데이타 가져오기
+                        MySqlCommand cmd = new MySqlCommand(sql, connection);
+                        //int a = cmd.ExecuteReader();
+
+                        MySqlDataReader table = cmd.ExecuteReader();
+
+                        //List<string> strList = new List<string>();
+
+
+                        //while (table.Read())
+                        //{
+                        //    this.ListBox.Items.Add(table["name"]);
+
+                        //    //strList.Add(table["name"]);
+                        //    //Console.WriteLine("{0} {1}", table["idx"], table["header"], table["body"]);
+                        //}
+                        table.Close();
+
+                    }
+                    connection.Close();
+                    select();
+                }
+                catch (Exception ex)
+                {
+                    this.ListBox.Items.Add(ex);
+
+                }
+
+            }
+        }
+
+        private void Init()
+        {
+            indexing = 0;
+
+            //throw new NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            indexing++;
+            this.ListBox.Items.Clear();
+            insert();
+            //select();
+        }
+    }
+}
